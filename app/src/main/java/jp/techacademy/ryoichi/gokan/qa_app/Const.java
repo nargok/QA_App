51131a59cf847f48904d8711d6fde4b0c50f71a2
後閑諒一 <nargok@gmail.com>
2017-07-25 11:45:44 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ryoichi/gokan/qa_app/Const.java
@@ -0,0 +1,12 @@
+package jp.techacademy.ryoichi.gokan.qa_app;
+
+/**
+ * Created by houxianliangyi on 2017/07/25.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users";       // FireBaseにユーザーの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // FireBaseに質問を保存するパス
+    public static final String AnswerPATH = "answers";    // FireBaseに回答を保存するパス
+    public static final String NameKEY = "name";          // Preferenceに表示名を保存する時のキー
+}
